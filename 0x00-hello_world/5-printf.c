b49c3a150155a01f30fa7d97cac5a35015c45746
seifkhald <133503170+seifkhald@users.noreply.github.com>
2023-06-16 02:11:12 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints with put function
+ * Description: A C program that prints with printf function.
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
+        printf("with proper grammar, but the outcome is a piece of art,\n");
         return (0);
 }
