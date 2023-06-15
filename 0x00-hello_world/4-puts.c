0ca66c2c844bf7c2650c7362a444e1f4620dee79
seifkhald <133503170+seifkhald@users.noreply.github.com>
2023-06-16 02:07:19 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,11 @@
  *
  * Description: A C program that prints with put function
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
 */
 
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
         return (0);
-}    
+}
