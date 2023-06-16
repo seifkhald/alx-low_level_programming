13bb74c8b225ed99dca68bd198568cf42052c092
seifkhald <seifkhald39@gmail.com>
2023-06-15 18:39:28 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints with put function
+ * Descripyion: A C program that prints with put function
  *
  * Return: Always 0 (Success)
 */
@@ -11,5 +11,5 @@
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+        return (0);
 }
