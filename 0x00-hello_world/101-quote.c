872383e6015b5ef97cf8ebdff561ce360cb8e2cb
seifkhald <seifkhald39@gmail.com>
2023-06-18 19:48:38 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,12 +6,12 @@
  * Description: print a qoute using write function
  *	write(int fd,const void *buf, size_t count)
  *
- * Return: Always 0 (success)
+ * Return: Always 1 (not success)
 */
 
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - dora Korpar, 2015-10-19\n";
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, quo, 59);
 	return (1);
