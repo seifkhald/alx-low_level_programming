46e2978136c5b5be4e75ba7f3ed308a9900f9dff
seifkhald <seifkhald39@gmail.com>
2023-06-29 18:03:09 -0700

    a

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,7 +6,7 @@
  * Return: string that is encoded
 */
 
-char *leet(char *)
+char *leet(char *c)
 {
 	char *cp = c;
 	char key[] = {'A', 'E', 'O', 'T', 'L'};
@@ -15,9 +15,8 @@ char *leet(char *)
 
 	while (*c)
 	{
-		for (i = 0; i < sizeof(key) / sizeof(char), i++)
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
 		{
-			/*32 is the difference between lower case letters and apper case letters*/
 			if (*c == key[i] || *c == key[i] + 32)
 			{
 				*c = 48 + value[i];
@@ -25,5 +24,6 @@ char *leet(char *)
 		}
 		c++;
 	}
+
 	return (cp);
 }
