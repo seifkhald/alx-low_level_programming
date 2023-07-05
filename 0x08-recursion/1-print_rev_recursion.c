ab0220b6bda412f2725a6626b9ad1899a341ed53
seifkhald <seifkhald39@gmail.com>
2023-07-05 15:29:00 -0700

    a

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - orints string in reverse
+ * @s: pointer to string
+ * *Returrn: void
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_print_rev_recusion(s + 1);
+		_putchar(*s);
+	}
+}
