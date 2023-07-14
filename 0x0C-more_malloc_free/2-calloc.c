3fda0e74f4827d95a04b1fd5a726787daca6c7d9
seifkhald <seifkhald39@gmail.com>
2023-07-13 17:37:52 -0700

    a

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,42 +1,27 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
- * *_memset - fills memory with a constant byte
- * @s: pointer to put the constant
- * @b: constant
- * @n: max bytes to use
- * Return: s
-*/
-
-char *_memset(char *s, char b, unsigned int n)
-{
-	char *ptr = s;
-
-	while (n--)
-		*s++ = b;
-
-	return (ptr);
-}
-
-/**
- * *_calloc - alocates memory for an array, using malloc
- * @nmemb: array length
- * @size: size of each element
- * Return: pointer
-*/
-
+ * _calloc - allocates memory for an array.
+ * @nmemb: number of elements.
+ * @size: size of bytes.
+ *
+ * Return: pointer to the allocated memory.
+ * if nmemb or size is 0, returns NULL.
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *m;
+	char *p;
+	unsigned int i;
 
-	if (size == 0 || nmemb == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	m = malloc(sizeof(int) * nmemb);
+	p = malloc(nmemb * size);
 
-	if (m == 0)
+	if (p == NULL)
 		return (NULL);
 
-	_memset(m, 0, sizeof(int) * nmemb);
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
 
-	return (m);
+	return (p);
 }
